78a92c2ab5709e5075a06795ab16a77b3063a0dc
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-02-20 19:39:06 -0600

    link to go back added

--- pages/walletError.tsx
+++ pages/walletError.tsx
@@ -1,13 +1,18 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
-const walletError = () => {
+const walletError: NextPage = () => {
 	return (
 		<>
-			<h1>Whoops</h1>
+			<h1 className="text-center mt-4">Whoops</h1>
 			<p className="text-center mt-4">
 				There was an error with your wallet. Please make sure you have installed
-				MetaMask extension ans try again.
-				<a href="/">return to the home page</a>.
+				MetaMask extension and try again.
+				<br />
+				<Link href="/">
+					<a className="underline decoration-solid text-teal-500">Go back</a>
+				</Link>
+				.
 			</p>
 		</>
 	);
