7995f101ca45d55727ca73a085596ca48c324b2e
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-02-22 14:06:34 -0600

    useRouter navigates to '/' removed

--- pages/walletError.tsx
+++ pages/walletError.tsx
@@ -1,8 +1,6 @@
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
 
 const walletError: NextPage = () => {
-	const router = useRouter();
 	return (
 		<>
 			<h1 className="text-center mt-4">Whoops</h1>
@@ -10,9 +8,7 @@ const walletError: NextPage = () => {
 				There was an error with your wallet. Please make sure you have installed
 				MetaMask extension and try again.
 				<br />
-				<button
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-					onClick={() => router.push('/')}>
+				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
 					Connect wallet
 				</button>
 			</p>
