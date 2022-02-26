5dd2f9b9f25a8c7c45427f1cc771fb7634aadc60
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-02-26 16:10:52 -0600

    export tailwind classes for better syntax

---
+++ components/tailwindClasses.ts
@@ -0,0 +1,4 @@
+export const theme = {
+	loginButton:
+		'inline-flex outline outline-1 outline-teal-400 items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 focus:shadow-outline hover:bg-teal-400 hover:text-black rounded',
+};
