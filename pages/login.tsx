ef286ba2fb73a3360ae9feeec3ba879143adaced
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-02-26 16:12:25 -0600

    Login page added

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,27 +1,19 @@
 import { NextPage } from 'next';
-import { AuthContext } from '../context/AuthProvider';
-import { useRouter } from 'next/router';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { theme } from '../components/tailwindClasses';
 
 const login: NextPage = () => {
-	const router = useRouter();
-	const { user, signIn } = useContext(AuthContext);
-
-	useEffect(() => {
-		if (user) {
-			router.push('/');
-		}
-	}, [user]);
+	const { signIn } = useContext(AuthContext);
 
 	return (
 		<>
-			<div className="flex justify-center">
-				<button
-					className="inline-flex outline outline-1 outline-teal-400 items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 focus:shadow-outline hover:bg-teal-400 hover:text-black rounded"
-					onClick={signIn}>
+			<div className="flex justify-center mt-5">
+				<button className={theme.loginButton} onClick={signIn}>
 					Sign in with Google
 				</button>
 			</div>
+			<div className="flex justify-center mt-5">hello from login</div>
 		</>
 	);
 };
