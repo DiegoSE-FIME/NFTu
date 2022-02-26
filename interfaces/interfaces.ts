acf7454bdc4fd76e86b067b475ae9ec2ef2fcafa
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-02-26 16:10:07 -0600

    interfaces for AuthContext added

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,13 +1,37 @@
-export interface User {
+export interface IAuthContext {
+	user: FirebaseData | null;
+	signIn: () => Promise<void>;
+	logOut: () => Promise<void>;
+	addUser: (user: FirebaseData) => void;
+	isLoading: boolean;
+}
+
+export interface FirebaseData {
 	uid: string;
 	email: string;
+	emailVerified?: boolean;
 	displayName: string;
+	isAnonymous?: boolean;
+	photoURL: string;
+	providerData?: ProviderDatum[];
+	stsTokenManager?: StsTokenManager;
+	createdAt?: string;
+	lastLoginAt?: string;
+	apiKey?: string;
+	appName?: string;
+}
+
+export interface ProviderDatum {
+	providerId: string;
+	uid: string;
+	displayName: string;
+	email: string;
+	phoneNumber: null;
 	photoURL: string;
 }
 
-export interface AuthContextType {
-	user: User | null;
-	signIn: () => Promise<void>;
-	signOut: () => Promise<void>;
-	addUser: (user: User) => void;
+export interface StsTokenManager {
+	refreshToken: string;
+	accessToken: string;
+	expirationTime: number;
 }
