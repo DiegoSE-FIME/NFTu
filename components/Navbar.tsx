b41ae762f5264412bf2d43ad704b418e6a00b9e5
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-02 14:08:59 -0600

    firebase user validations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,16 @@
 import styles from './Logo.module.css';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useCallback, useContext } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { connector } from '../config/web3';
 import { useRouter } from 'next/router';
 import { AuthContext } from '../context/AuthContext';
+import { theme } from './tailwindClasses';
 
 const Navbar = () => {
 	const router = useRouter();
 	const { firebaseUser, signOutUser } = useContext(AuthContext);
-	const { activate, active, deactivate, error, account, chainId } =
-		useWeb3React();
+	const { activate, active, deactivate, error } = useWeb3React();
 
 	const connectWallet = useCallback(() => {
 		activate(connector);
@@ -28,7 +27,7 @@ const Navbar = () => {
 	}
 
 	return (
-		<div>
+		<>
 			<nav className="px-2 sm:px-4 py-2.5">
 				<div className="container flex flex-wrap justify-between items-center mx-auto ">
 					<Link href="/">
@@ -37,9 +36,9 @@ const Navbar = () => {
 						</a>
 					</Link>
 
-					<div className="flex items-center justify-center nav-links">
+					<div className="flex items-center xl:ml-24 justify-center nav-links hidden xl:inline-flex">
 						<Link href="/marketplace">
-							<a className="px-2 py-1 text-sm  text-white-500 hover:text-teal-200">
+							<a className="px-2 py-1 text-sm  text-white-500 hover:text-teal-200 hidden xl:inline-flex">
 								Marketplace
 							</a>
 						</Link>
@@ -59,14 +58,14 @@ const Navbar = () => {
 					<div className="space-x-2">
 						{active ? (
 							<button
-								className="inline-flex items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 hidden xl:inline-flex"
+								className={theme.navbarLoginButton}
 								onClick={disconnectWallet}>
 								{' '}
 								Disconnect wallet{' '}
 							</button>
 						) : (
 							<button
-								className="inline-flex items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 bg-indigo-600 rounded-lg focus:shadow-outline hover:bg-indigo-800 hidden xl:inline-flex"
+								className={theme.navbarLoginButton}
 								onClick={connectWallet}>
 								{' '}
 								Connect wallet{' '}
@@ -74,15 +73,13 @@ const Navbar = () => {
 						)}
 
 						{firebaseUser ? (
-							<button
-								className="inline-flex items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 bg-gray-500 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-								onClick={signOutUser}>
+							<button className={theme.signOutButton} onClick={signOutUser}>
 								{' '}
 								Sign out{' '}
 							</button>
 						) : (
 							<button
-								className="inline-flex items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 bg-indigo-600 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+								className={theme.signInButton}
 								onClick={() => router.push('/Login')}>
 								{' '}
 								Sign In{' '}
@@ -91,7 +88,7 @@ const Navbar = () => {
 					</div>
 				</div>
 			</nav>
-		</div>
+		</>
 	);
 };
 
