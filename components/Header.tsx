fdafcc065172a85cf41e2c02a2733690839d251f
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-01 22:52:28 -0600

    styles added from tailwinClasses.tsx for better syntax

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,44 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Header.module.css';
+import { theme } from './tailwindClasses';
+import { AuthContext } from '../context/AuthContext';
+import { useContext } from 'react';
 
 const Header = () => {
+	const { firebaseUser } = useContext(AuthContext);
 	return (
-		<div className="grid grid-cols-2 gap-4">
-			<div className="w-40 absolute left-[30%] top-[22%] self-auto hidden xl:block">
+		<div className={theme.headerDiv}>
+			<div className={theme.headerStars}>
 				<Image src="/Star2.png" alt="Star2" width={50} height={50} />
 				<Image src="/Star.png" alt="Star" width={20} height={20} />
 			</div>
-			<div className="xl:w-[450px] w-[300px] col-span-1 ml-5 sm:ml-[50px] mt-[20%]">
-				<h1 className="text-3xl font-semibold sm:text-5xl sm:leading-normal leading-normal ">
+			<div className={theme.headerTitleDiv}>
+				<h1 className={theme.headerTitle}>
 					A platform to find your digital unique rarity
 				</h1>
-				<p className="text-gray-400 mt-5">
+				<p className={theme.headerDescription}>
 					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
 					voluptatem expedita asperiores voluptate quaerat cupiditate unde ipsa,
 					natus eum inventore beatae quos quod nam, accusamus molestiae ut alias
 					placeat vitae.
 				</p>
-				{/* outline button that says Get started and on the right side a How it works with only border in the bottom*/}
+
 				<div className="mt-5">
-					<Link href="/login">
-						<button className="inline-flex outline outline-1 outline-teal-400 items-center h-10 px-5 mt-2.5 text-indigo-100 transition-colors duration-150 focus:shadow-outline hover:bg-teal-400 hover:text-black">
+					<Link href={firebaseUser ? '/marketplace' : '/Login'}>
+						<button className={theme.getStartedButton}>
 							Get started
 							<Image src="/assets/Line.png" alt="line" width={20} height={20} />
 						</button>
 					</Link>
 					<Link href="/">
-						<a className="inline-flex border-b-2 border-teal-400 ml-7 font-thin">
-							How it works
-						</a>
+						<a className={theme.howItWorksButton}>How it works</a>
 					</Link>
 				</div>
 			</div>
+
 			<div className={styles.image}>
-				<div className="w-40 z-40 absolute top-[19%] right-[32%] hidden xl:block">
+				<div className={theme.headerImage}>
 					<Link href="/marketplace">
 						<a>
 							<Image
@@ -47,6 +50,7 @@ const Header = () => {
 						</a>
 					</Link>
 				</div>
+
 				<div className={styles.info_image}>
 					<Image src="/InfoImage.png" width={300} height={300} alt="image" />
 				</div>
