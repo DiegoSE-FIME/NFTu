474b5d5fbee4412af5363423e96ea05e4a8d4d40
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-01 22:57:01 -0600

    User auth changed with firebase state

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -7,85 +7,97 @@ import {
 	provider,
 	signInWithPopup,
 	signOut,
-	onAuthStateChanged,
+	addDoc,
+	collection,
 } from '../config/firebase/firebase.js';
 
-import { ref, set } from 'firebase/database';
-import { FirebaseData } from '../interfaces/interfaces';
+import { FirebaseData, IUser } from '../interfaces/interfaces';
 
 type AuthProviderProps = {
 	children: JSX.Element | JSX.Element[];
 };
-
-export interface IUser {
-	uid: string;
-	email: string;
-	displayName: string;
-	photoURL: string;
-}
+// User authentication with SignInWithGoogle saving user data to firestore database and local storage and SignOut
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-	const initalUserData = {
-		uid: '',
-		email: '',
-		displayName: '',
-		photoURL: '',
-		createdAt: '',
-	};
-	const [user, setUser] = useState<FirebaseData>(initalUserData);
+	const [firebaseUser, setFirebaseUser] = useState<IUser | null>(null);
+	const [firebaseData, setFirebaseData] = useState<FirebaseData | null>(null);
 	const [isLoading, setIsLoading] = useState(true);
 
-	const router = useRouter();
-
-	useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-			if (user) {
-				setUser(user as FirebaseData);
-				setIsLoading(false);
-			} else {
-				setUser(initalUserData);
-				setIsLoading(false);
-			}
-		});
-	}, [user]);
-
-	const signIn = async () => {
-		await signInWithPopup(auth, provider);
-		const firebaseUser = auth.currentUser;
-		setUser({
-			uid: firebaseUser?.uid || '',
-			email: firebaseUser?.email || '',
-			displayName: firebaseUser?.displayName || '',
-			photoURL: firebaseUser?.photoURL || '',
-			createdAt: new Date().toISOString(),
-		});
+	const signInWithGoogle = async () => {
+		const result = await signInWithPopup(auth, provider);
+		if (result.user) {
+			const user = result.user;
+			const userData: IUser = {
+				uid: user?.uid,
+				email: user?.email || '',
+				displayName: user?.displayName || '',
+				photoURL: user?.photoURL || '',
+				createdAt: user.metadata?.creationTime || '',
+			};
+			setFirebaseUser(userData);
+			console.log(userData);
+			setIsLoading(false);
+		}
 	};
 
-	const logOut = async () => {
-		signOut(auth);
-		setUser(initalUserData);
-		router.push('/');
+	const signOutUser = async () => {
+		await signOut(auth);
+		setFirebaseUser(null);
+		setIsLoading(false);
 	};
 
 	const addUser = async (user: IUser) => {
-		set(ref(db, 'users'), {
+		const userData: FirebaseData = {
 			uid: user.uid,
 			email: user.email,
 			displayName: user.displayName,
 			photoURL: user.photoURL,
-			createdAt: new Date().toISOString(),
-			lastLoginAt: new Date().toISOString(),
-		});
+			createdAt: user.createdAt,
+		};
+		await addDoc(collection(db, 'users'), userData);
 	};
+
+	useEffect(() => {
+		auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				const userData: IUser = {
+					uid: user?.uid,
+					email: user?.email || '',
+					displayName: user?.displayName || '',
+					photoURL: user?.photoURL || '',
+					createdAt: user.metadata?.creationTime || '',
+				};
+				setFirebaseUser(userData);
+				await addUser(userData);
+				setIsLoading(false);
+			} else {
+				setFirebaseUser(null);
+				setIsLoading(false);
+			}
+		});
+	}, []);
+
+	useEffect(() => {
+		if (firebaseUser) {
+			const userData: FirebaseData = {
+				uid: firebaseUser.uid,
+				email: firebaseUser.email,
+				displayName: firebaseUser.displayName,
+				photoURL: firebaseUser.photoURL,
+				createdAt: firebaseUser.createdAt,
+			};
+			setFirebaseData(userData);
+		}
+	}, [firebaseUser]);
 
 	return (
 		<AuthContext.Provider
 			value={{
-				user,
+				firebaseUser,
+				firebaseData,
 				isLoading,
-				signIn,
-				logOut,
-				addUser,
+				signInWithGoogle,
+				signOutUser,
 			}}>
 			{children}
 		</AuthContext.Provider>
