e6c5ce33361bbae143ffc33ffa32f5d2939246c8
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-04 11:28:05 -0600

    Tailwind responsive classes added

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,9 +1,34 @@
-const Body = () => {
+import Image from 'next/image';
+import { theme } from './tailwindClasses';
+
+const Header = () => {
 	return (
-		<div>
-			<h1 className="mt-5 text-center">Hello from body</h1>
+		<div className={theme.bodyDiv}>
+			<div className="hidden xl:block xl:px-20 xl:py-11">
+				<Image src="/Card.png" alt="Card" width={350} height={400} />
+				<button className={theme.placeABidButton}>
+					<span>Place a bid</span>
+				</button>
+			</div>
+
+			<div>
+				<div className={theme.bodyTitle}>
+					<h2>Build on ETH.</h2>
+					<h2>
+						Owned by <span className="text-teal-400">you</span>.
+					</h2>
+				</div>
+				<div className="xl:mt-5 xl:w-[500px] w-96 px-5 xl:px-0">
+					<p className={theme.headerDescription}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+						cum ullam obcaecati non possimus! Nihil inventore aspernatur
+						corporis quia architecto et voluptate molestias officia, illo
+						doloribus. Nemo ex quaerat nihil.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Body;
+export default Header;
