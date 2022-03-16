1f5abcefe4c2e18109463090f5525cfcd8f179af
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-16 13:23:01 -0600

    New ownerAddress added

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -8,7 +8,7 @@ import { DataResponse, OwnedNft } from '../interfaces';
 const Marketplace: NextPage = () => {
 	const apiKey: Key = 'z6-SaAgm7QbsR3nNeob_oO6iIfHcLIV-';
 	const baseURL: string = `https://eth-mainnet.alchemyapi.io/v2/${apiKey}/getNFTs`;
-	const ownerAddr: string = '0x7d5498a12b53259a2362fc233fe4d6b5d7bf3400';
+	const ownerAddr: string = '0x7217bc604476859303a27f111b187526231a300c';
 	const options = {
 		method: 'GET',
 		url: `${baseURL}?=owner=${ownerAddr}`,
@@ -19,7 +19,7 @@ const Marketplace: NextPage = () => {
 	const { url } = options;
 	const { data } = useFetchNFT(url);
 	const { ownedNfts } = (data as DataResponse) || '';
-
+	console.log(ownedNfts);
 	return !data ? (
 		<h2 className="flex justify-center">Loading...</h2>
 	) : (
@@ -28,10 +28,10 @@ const Marketplace: NextPage = () => {
 				<h1 className="text-center">Marketplace</h1>
 				<div className="flex justify-center">
 					<div className="xl:grid xl:gap-x-8 xl:gap-y-4 xl:grid-cols-3 mt-11">
-						{ownedNfts.map((nft: OwnedNft) => (
+						{ownedNfts.slice(18, 22).map((nft: OwnedNft) => (
 							<Card
 								key={nft.id.tokenId}
-								image={nft.media[0].raw}
+								image={nft.media[0].gateway}
 								title={nft.title}
 								name="NFTu"
 								address={nft.contract.address}
