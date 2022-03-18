1ef1bbd4cdfea6600e6895f615991650d27d1f0e
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-18 12:15:33 -0600

    tailwindClasses and Loader component styles added

---
+++ theme/loader.ts
@@ -0,0 +1,26 @@
+export const containerStyle = {
+	position: 'relative',
+	width: '3rem',
+	height: '3rem',
+	boxSizing: 'border-box',
+	overflow: 'hidden',
+};
+
+export const circleStyle = {
+	display: 'block',
+	width: '3rem',
+	height: '3rem',
+	border: '0.5rem solid #e9e9e9',
+	borderTop: '0.5rem solid #41EB5C',
+	borderRadius: '50%',
+	position: 'absolute',
+	boxSizing: 'border-box',
+	top: 0,
+	left: 0,
+};
+
+export const spinTransition = {
+	loop: Infinity,
+	ease: 'linear',
+	duration: 1,
+};
