74c85414ba2ad156f67092974b719579d7e039fc
DiegoSE-FIME <ve06976@innovaccion.mx>
2022-03-27 16:26:05 -0600

    image exporting

---
+++ utils/descriptions.ts
@@ -0,0 +1,4 @@
+export const descriptions = {
+	example:
+		'CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and became one of the inspirations for the ERC-721 standard.',
+};
---
+++ utils/images.ts
@@ -0,0 +1,20 @@
+export const images: string[] = [
+	'/forYou.png',
+	'/forYou2.png',
+	'/forYou3.png',
+	'/forYou4.png',
+];
+
+export const topArtistsImages: string[] = [
+	'/topArtists.png',
+	'/topArtists2.png',
+	'/topArtists3.png',
+	'/topArtists4.png',
+];
+
+export const exampleImages: string[] = [
+	'/example.png',
+	'/example2.png',
+	'/example3.png',
+	'/example4.png',
+];
